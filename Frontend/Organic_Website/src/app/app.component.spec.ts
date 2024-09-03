597f4efa6ad531f42b3b70a2012137073afa68ac
basilahamed07 <basilahamed46@gmail.com>
2024-09-04 01:15:43 +0530

    angulare 1 percentage work done

--- Frontend/Organic_Website/src/app/app.component.spec.ts
+++ Frontend/Organic_Website/src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Organic_Website'`, () => {
+  it(`should have the 'Organic_Website' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Organic_Website');
--- Frontend/Organic_Website/src/app/app.component.ts
+++ Frontend/Organic_Website/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './Main/home/home.component';
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
